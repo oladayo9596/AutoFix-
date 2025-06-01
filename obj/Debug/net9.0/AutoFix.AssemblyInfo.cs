d5ae4857b36df75f360d69b3a11de4f1392b9f2e
Oladele Oladayo <oladayooladele46@gmail.com>
2025-06-01 20:31:39 +0100

    commit

--- obj/Debug/net9.0/AutoFix.AssemblyInfo.cs
+++ obj/Debug/net9.0/AutoFix.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoFix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502ee140ca90d18d2955458be69ee672f302015d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532e69593ccb207ab4f72bcfbe81f02bab90ff01")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoFix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoFix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
